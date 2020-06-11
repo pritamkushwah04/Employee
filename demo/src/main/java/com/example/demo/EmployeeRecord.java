88aaff355cdb234a1ecd7fec1e199c67c4ec2490
niharika-kushwah <38259887+niharika-kushwah@users.noreply.github.com>
2020-06-11 14:53:11 +0530

    Added getEmployee method

--- demo/src/main/java/com/example/demo/EmployeeRecord.java
+++ demo/src/main/java/com/example/demo/EmployeeRecord.java
@@ -17,10 +17,13 @@ public class EmployeeRecord {
 		return eRec;
 	}
 
+	// method to add an employee
 	public void addEmployee(Employee e) {
 		edata.add(e);
 	}
 
+	
+	//method to delete an employee 
 	public Boolean deleteEmp(Employee e) {
 		for (int i = 0; i < edata.size(); i++) {
 			Employee emp = edata.get(i);
@@ -31,8 +34,22 @@ public class EmployeeRecord {
 		}
 		return false;
 	}
+	
+	  
+	 // Method to get Employee object according to given id
+	  public Employee geEmployee(int id) {
+		  Employee e=null;
+		  for (int i=0;i<edata.size();i++) {
+			  e=edata.get(i);
+			  if(id == e.geteID()) {
+				  return e;
+			  }
+		  }
+		  return e;
+	  }
 
 	
+	  // method to update existing employee 
 	  public Boolean updateEmployee(Employee e){
 	  
 	  for (int i=0;i<edata.size();i++) { 
@@ -45,7 +62,7 @@ public class EmployeeRecord {
 	  return false;
 	  }
 	 
-
+    // method to get all employee list
 	public List<Employee> getEmployeeRecord() {
 		return edata;
 	}
